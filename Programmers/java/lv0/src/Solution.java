73722f555635f4c36ebdaa16c8338c420911e030
문영수 <moonyoungsu@naver.com>
2025-03-24 21:24:59 +0900

    로컬 소스 반영

---
+++ Programmers/java/lv0/src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        long[] answer = new long[5];
+
+        for ( int i = 0 ; i<5; i++) {
+            answer[i] = 2*(i+1);
+            System.out.println(answer[i]);
+        }
+
+    }
+}
